f6006ef75ab9484e2830d7e3d7b3c6e71471b0df
Lorenzo Fusari <lorenzo.fusari@theoaklandgroup.co.uk>
2022-07-28 16:31:12 +0100

    steps 1 to 4

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,16 @@
+import re
+
+pattern = re.compile(r'\/\/(.+)\\n(.+)')
+
 def Add(numbers):
-    parsed_numbers = int(numbers)
-    return parsed_numbers
+    if len(numbers) == 0:
+        return 0 
+    if numbers[0:2] == '//':
+        for match in pattern.finditer(numbers):
+            re_delimiter = match.group(1)
+            re_numbers = match.group(2)
+            return re_delimiter, re_numbers
+    number_list= numbers.replace("\n", ",").split(',')
+    int_list = [int(num) for num in number_list]
+   
+    return sum(int_list)
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,6 +3,43 @@
 from src.calculator import Add
 
 
-def test_add_empty_string():
+def test_add_zero():
     result = Add('0')
     assert result == 0
+
+
+def test_add_empty_string():
+    result = Add('')
+    assert result == 0
+
+
+def test_add_two_numbers():
+    result = Add('1,2')
+    assert result == 3
+
+
+def test_add_unknown_numbers():
+    result_1 = Add('90,18,1987,1,0')
+    result_2 = Add('20,3,15,6,100')
+    result_3 = Add('324567,21,43,17,901')
+    assert result_1 == 2096
+    assert result_2 == 144
+    assert result_3 == 325549
+
+
+def test_add_numbers_with_newlines():
+    result = Add('1\n2,3')
+    assert result == 6
+
+
+def test_add_numbers_exception():
+    try:
+        result = Add('1,\n')
+        assert False
+    except:
+        assert True
+
+
+def test_add_numbers_different_delimiters():
+    result = Add("//;\n1;2")
+    assert result == 3
